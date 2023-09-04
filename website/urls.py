c5242fa23eb29a366ea3c6a8a9b2d5a9a4be9f49
mickey1070 <dasarivamsi01@gmail.com>
2023-09-04 14:44:22 +0530

    user_auth

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home,name='home.html'),
-    
+    path('', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user'),
+    path('register/', views.register_user, name='register_user'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
